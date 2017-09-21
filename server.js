2e07ff6fd3fb0c45ee26152bbee5262dd43b7a12
Eric Washington <Washington_Eric@bah.com>
2017-09-21 01:09:45 -0400

    Server.js

--- server.js
+++ server.js
@@ -1,30 +1,67 @@
-    // grab everything we need
+const startButton = document.querySelector('[data-action="start"]');
+const stopButton = document.querySelector('[data-action="stop"]');
+const resetButton = document.querySelector('[data-action="reset"]');
+const minutes = document.querySelector('.minutes');
+const seconds = document.querySelector('.seconds');
 
-    const priceInput    = document.querySelector('[name=price]');
-    const quantityInput = document.querySelector('[name=quantity]');
-    const total         = document.querySelector('.total');
-    const quantityLabel = document.querySelector('.quantity-label');
+///////////////////////////////////////////////////////////////////////
 
-    // create the functions that we'll need   
+//Prevents timer from being clicked twice after you start it
+let isRunning = false;
+//Starts timer at 00
+let timerTime = 00;
+let interval;
 
-    function calculateShoeCost() {
-      const price    = priceInput.value;
-      const quantity = quantityInput.value;
-      const cost     = price * quantity;
-      console.log(cost);
-      total.innerText = '$' + cost.toFixed(2);
+///////////////////////// Functions ////////////////////////////////
+
+//Start the timer
+function startTimer() {
+    if (isRunning) return;
+    isRunning = true;
+    interval = setInterval(incrementTimer, 1000);
+
+}
+
+//Stop the timer
+function stopTimer() {
+    if (!isRunning) return;
+    isRunning = false;
+    clearInterval(interval);
+}
+
+//Reset the Timer
+function resetTimer() {
+    stopTimer();
+    timerTime = 0;
+    seconds.innerText = '00';
+    minutes.innerText = '00';
+}
+
+//Increment the Stopwatch
+function incrementTimer() {
+    timerTime += 1;
+
+    const numOfMinutes = Math.floor(timerTime / 60);
+    const numOfSeconds = timerTime % 60;
+
+    seconds.innerText = pad(numOfSeconds);
+    minutes.innerText = pad(numOfMinutes);
+}
+
+//Adds Number in the form of a string to the Stopwatch 
+function pad(number) {
+    //Ternary Operator
+    // return (number < 10) ? '0' + number : number;
+
+    if (number < 10) {
+        return '0' + number; 
+    } else {
+        return number;
     }
-    function updateQuantityLabel() {
-      const quantity = quantityInput.value;
-      quantityLabel.innerText = quantity;
-    }
+}
 
-    // on first run
+/////////////////////////// Event Listeners ///////////////////////////
 
-    calculateShoeCost();
-
-    // add our event listeners  
-       
-    priceInput.addEventListener('input', calculateShoeCost);
-    quantityInput.addEventListener('input', calculateShoeCost);
-    quantityInput.addEventListener('input', updateQuantityLabel);
+startButton.addEventListener('click', startTimer);
+stopButton.addEventListener('click', stopTimer);
+resetButton.addEventListener('click', resetTimer);
